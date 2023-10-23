a902c1ea31bbb8f45909fc5d9bd1c3aa4d0d8962
JAgui13 <146782407+JAgui13@users.noreply.github.com>
2023-10-23 14:38:57 -0700

    ArrayExample Bug Fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+   @Test
+  public void testReversedMultiple() {
+    int[] input1 = { 1,2,3 };
+    assertArrayEquals(new int[]{3,2,1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlaceMultiple() {
+    int[] input1 = { 3,2,1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1,2,3 }, input1);
+	}
 }
